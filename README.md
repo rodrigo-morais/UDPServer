c9216be3a7f57110f50de25f6f0591477917c48b
Rodrigo Morais <morais.rm@gmail.com>
2013-05-31 19:41:36 -0300

    Criando repositório.

---
+++ README.md
@@ -0,0 +1,4 @@
+UDP Server
+===========================
+
+Aplicativo que simula um servidor em uma troca de mensagens utilizando socket e o protocolo UDP.
---
+++ UPDServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UPDServer
+{
+    class Program
+    {
+        private static UdpClient server;
+
+        static void Main(string[] args)
+        {
+            server = new UdpClient(9999);
+            Console.WriteLine("Server connection");
+
+            Console.Write(">> ");
+
+            while (true)
+            {
+                receive();
+            }
+        }
+
+        private static void receive()
+        {
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 9999);
+            byte[] data = new byte[1024];
+            data = server.Receive(ref sender);
+            String info = Encoding.ASCII.GetString(data, 0, data.Length);
+            Console.WriteLine(info);
+            Console.Write(">> ");
+        }
+    }
+}
